84a1fe40508bb01abf0ef901538cfbb0c96433e0
NGtrongNGWork <ngtrongng.worknu1@gmail.com>
2026-09-02 22:04:34 +0700

    bill_mangament

---
+++ FrontEnd/src/cardBill.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CardBill()
+{
+    return(
+        // Thẻ chứa thông tin hóa đơn
+        <div className="cardBill" style={{width : "200px", height : "300px", borderRadius : "15px", backgroundColor :"#FED942", border: "3px solid #fff"}}>
+
+        </div>
+    )
+}
+export default CardBill
