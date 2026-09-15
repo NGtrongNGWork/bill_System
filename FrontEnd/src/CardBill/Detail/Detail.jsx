94e6edb5576c31b9782bf94d56d23b63fa585f04
NGtrongNGWork <ngtrongng.worknu1@gmail.com>
2026-09-15 09:34:09 +0700

    Thêm icon, Chi tiết Tổng Quan

--- FrontEnd/src/CardBill/Detail/Detail.jsx
+++ FrontEnd/src/CardBill/Detail/Detail.jsx
@@ -3,8 +3,12 @@ import './Detail.css'
 function Details()
 {
     return(
-        <div className="container">
-
+        <div className="Details">
+            <div className="container">
+                <div className="Inform_Img">Thông tin hình ảnh <button className="ShowDetail"><img src="./src/img/icon/down.png" alt="" /></button></div>
+                <div className="Inform_Bill">Thông tin hóa đơn <button className="ShowDetail"><img src="./src/img/icon/down.png" alt="" /></button></div>
+                <div className="Inform_Order">Thông tin đơn hàng <button className="ShowDetail"><img src="./src/img/icon/down.png" alt="" /></button></div>
+            </div>
         </div>
     )
 }
