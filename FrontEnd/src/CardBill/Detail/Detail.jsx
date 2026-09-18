149f8e1b4e37643389501b816e77a04e86b78e76
NGtrongNGWork <ngtrongng.worknu1@gmail.com>
2026-09-18 14:02:24 +0700

    Thiếp lập sự kiện đóng mở item V0

--- FrontEnd/src/CardBill/Detail/Detail.jsx
+++ FrontEnd/src/CardBill/Detail/Detail.jsx
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import './Detail.css'
-function Details()
-{
-    return(
+function Details() {
+    const [MoreDetail, SetMoreDetail] = useState(false)
+    const ClickShow = () => {
+        if (MoreDetail) SetMoreDetail(false);
+        else SetMoreDetail(true);
+    }
+    return (
         <div className="Details">
             <div className="container">
-                <div className="Inform_Img">Thông tin hình ảnh <button className="ShowDetail"><img src="./src/img/icon/down.png" alt="" /></button></div>
-                <div className="Inform_Bill">Thông tin hóa đơn <button className="ShowDetail"><img src="./src/img/icon/down.png" alt="" /></button></div>
-                <div className="Inform_Order">Thông tin đơn hàng <button className="ShowDetail"><img src="./src/img/icon/down.png" alt="" /></button></div>
+                <div className="Inform_Card">
+                    <div className="btn_Show">
+                    <p className="Text_Inform">Thông tin hình ảnh</p>
+                    <button className="ShowDetail" onClick={() => { ClickShow() }}><img src={MoreDetail == true?'./src/img/icon/right-arrows.png':'./src/img/icon/down.png'} alt="" />
+                    </button>
+                
+                    </div>
+                {MoreDetail == true?<>
+                <div className="MoreDetail" style={{ backgroundColor: 'red', width: '100%', height: '200px', display:"flex",justifyContent:'center' }}></div></> : null
+                }
+                </div>
+            <div className="Inform_Card">
+                    <div className="btn_Show">
+                    <p className="Text_Inform">Thông tin hình ảnh</p>
+                    <button className="ShowDetail" onClick={() => { ClickShow() }}><img src={MoreDetail == true?'./src/img/icon/right-arrows.png':'./src/img/icon/down.png'} alt="" />
+                    </button>
+                
+                    </div>
+                {MoreDetail == true?<>
+                <div className="MoreDetail" style={{ backgroundColor: 'red', width: '100%', height: '200px', display:"flex",justifyContent:'center' }}></div></> : null
+                }
+                </div>
+                <div className="Inform_Card">
+                    <div className="btn_Show">
+                    <p className="Text_Inform">Thông tin hình ảnh</p>
+                    <button className="ShowDetail" onClick={() => { ClickShow() }}><img src={MoreDetail == true?'./src/img/icon/right-arrows.png':'./src/img/icon/down.png'} alt="" />
+                    </button>
+                
+                    </div>
+                {MoreDetail == true?<>
+                <div className="MoreDetail" style={{ backgroundColor: 'red', width: '100%', height: '200px', display:"flex",justifyContent:'center' }}></div></> : null
+                }
+                </div>
             </div>
         </div>
     )
